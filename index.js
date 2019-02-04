f8c64d554986613b77d13a511738929db31b2237
gkarnati3 <gkarnati3@gatech.edu>
2019-02-03 22:14:58 -0500

    toggle svgs except script in html because external link is not working

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ const port = 3000;
 app.use('/', express.static('./public'));
 
 app.listen(port, () => console.log(`Listening on ${port}`));
+
+function toggleFloors() {
+    var top = document.getElementById("floor1");
+    var bottom = document.getElementById("floor2");
+    if (top.style.display === "none") {
+    top.style.display = "block";
+    bottom.style.display = "none";
+    } else {
+    top.style.display = "none";
+    bottom.style.display = "block";
+    }
+}
