9b0f622c9cc0c20d43daa58608dad15731cd7aed
Elton <eltonp3103@gmail.com>
2019-02-18 00:07:05 -0500

    fixed toggle bug

--- public/util.js
+++ public/util.js
@@ -1,60 +1,102 @@
 var firstFloor = document.getElementById("floor1");
 var secondFloor = document.getElementById("floor2");
 
+var rooms = [{
+        "number": 103,
+        "name": "",
+        "floor": 1,
+        "image": ""
+    },
+    {
+        "number": 105,
+        "name": "",
+        "floor": 1,
+        "image": ""
+    },
+    {
+        "number": 109,
+        "name": "",
+        "floor": 1,
+        "image": ""
+    },
+    {
+        "number": 111,
+        "name": "",
+        "floor": 1,
+        "image": ""
+    },
+    {
+        "number": 115,
+        "name": "",
+        "floor": 1,
+        "image": ""
+    },
+    {
+        "number": 205,
+        "name": "",
+        "floor": 2,
+        "image": ""
+    },
+    {
+        "number": 209,
+        "name": "",
+        "floor": 2,
+        "image": ""
+    },
+    {
+        "number": 211,
+        "name": "",
+        "floor": 2,
+        "image": ""
+    },
+    {
+        "number": 215,
+        "name": "",
+        "floor": 2,
+        "image": ""
+    }
+]
+
+function addLocationPins(floor) {
+    // adds location pins to all the rooms for the given floor
+
+    for (let i = 0; i < rooms.length; i += 1) {
+        if (rooms[i]["floor"] === floor) {
+            addPinToLoc(rooms[i]["number"]);
+        }
+    }
+}
+
 function addPinToLoc(roomNumber) {
-// adds a location pin for the specified room number in the map
+    // adds a location pin for the specified room number in the map
 
-      var stringRoomNumber = String(roomNumber);
-      var floorNumber = parseInt(stringRoomNumber.charAt(0));
-
-      // get the floor element
-      var floorElement = floorNumber == 2 ? secondFloor : firstFloor;
-
-      floorElement.addEventListener("load", () => {
-        // accessing SVG dom of the map
+    var stringRoomNumber = String(roomNumber);
+    var floorNumber = parseInt(stringRoomNumber.charAt(0));
+    var floorElement = floorNumber == 2 ? secondFloor : firstFloor;
+    floorElement.addEventListener("load", () => {
         var svgDOM = floorElement.contentDocument.children[0]
-        // finding matching text nodes for given room number
         var matchingTextNodes = Array.from(svgDOM.querySelectorAll('text'))
-        .find(el => el.textContent.trim() === stringRoomNumber.trim());
-        // get the location coordinates within the SVG
+            .find(el => el.textContent.trim() === stringRoomNumber.trim());
         // x offset +4
         // y offset -25
         var xpos = matchingTextNodes.getAttribute("x") - -4;
+        // I know this looks weird, but javascript is more weird
+        // if i do xpos + 4, it just doesnt work, but - works
         var ypos = matchingTextNodes.getAttribute("y") - 25;
-        console.log(xpos);
-        console.log(ypos);
-
         var pin = createPin(roomNumber, location);
-
         var pins = document.getElementById("pins");
         pins.appendChild(pin);
         pin.addEventListener("load", () => {
             pinDOM = pin.contentDocument.children[0];
-            console.log("reached")
             pinDOM.setAttribute("x", xpos);
             pinDOM.setAttribute("y", ypos);
             svgDOM.appendChild(pinDOM);
         }, false);
-
     }, false);
 }
 
-function addLocationPins(floor) {
-// adds location pins to all the rooms for the given floor
-
-    $.getJSON("rooms.json", function(json) {
-        for (let i = 0; i < json.length; i += 1 ) {
-            if (json[i]["floor"] === floor){
-                addPinToLoc(json[i]["number"]);
-            }
-        }
-    })
-}
-
 function setPinAttributes(pin, roomNumber, location) {
-// setting attributes for the pin element to be added
-
-// TODO: Refactor to add the pin to the svg
+    // setting attributes for the pin element to be added
 
     var attributes = {
         "id": roomNumber,
@@ -64,47 +106,28 @@ function setPinAttributes(pin, roomNumber, location) {
     for (let key in attributes) {
         pin.setAttribute(key, attributes[key]);
     }
-    pin.setAttribute('pointer-events', 'none');
-    pin.classList.add("location-pin");
-    pin.style.left = location['left'] + "px";
-    pin.style.top = location['top'] + "px";
-
 }
 
 function createPin(roomNumber, location) {
-// creates a pin element that will be added to the map svg
+    // creates a pin element that will be added to the map svg
 
-    console.log("reached create Pin");
     var pin = document.createElement("object");
     setPinAttributes(pin, roomNumber, location);
     return pin
 }
 
 function toggleFloors() {
-// toggling between the maps for floors 1 and 2
+    // toggling between the maps for floors 1 and 2
 
-      if (firstFloor.style.display === "none") {
-            firstFloor.style.display = "block";
-            secondFloor.style.display = "none";
-      } else {
-            firstFloor.style.display = "none";
-            secondFloor.style.display = "block";
-      }
+    if (firstFloor.style.display === "none") {
+        firstFloor.style.display = "block";
+        secondFloor.style.display = "none";
+        addLocationPins(1);
+    } else {
+        firstFloor.style.display = "none";
+        secondFloor.style.display = "block";
+        addLocationPins(2);
+    }
 }
 
-
-
-// adds the pins for floor 1
-
-// TODO: make a call based on which floor is chosen
 addLocationPins(1);
-
-// function toggleCard(top) {
-//     if (card.style.height == "250px") {
-//         $(card).animate({height: "0px"});
-//         $(card).animate({top: "10px"});
-//     } else {
-//         $(card).animate({height: "250px"}, "fast", "swing");
-//         $(card).animate({top: "250px"}, "fast", "swing");
-//     }
-// }
